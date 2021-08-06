66f6824baf7443df09d62c11b8d79276f4062bf3
Yang Kyung Hun <88442236+ygh1254@users.noreply.github.com>
2021-08-06 22:40:00 +0900

    Create Greedy Algorithm 3-3 (1).py

---
+++ Greedy Algorithm 3-3 (1).py
@@ -0,0 +1,12 @@
+# Greedy Algorithm Problem 2 (1)
+n, m = map(int, input().split())
+result = 0
+
+for _ in range(m) :
+    data.append(list(map(int, input().split())))
+    min_value = min(data) # Find minimum value in data.
+    result = max(result, min_value) # Find max value among the minimum value and variation result
+
+print(result)
+
+# By using min function and max function. Much more brief code!
