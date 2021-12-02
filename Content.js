58e76ea45e2aac25fe515c65f573ea52c5233065
Yang Kyung Hun <88442236+ygh1254@users.noreply.github.com>
2021-12-03 01:47:19 +0900

    Update Content.js

--- Content.js
+++ Content.js
@@ -2,26 +2,89 @@ import React, { Component } from 'react';
 
 class Content extends Component{
     render(){
-        var data = this.props.data;
-        var i = 0;
+        var data=this.props.data;
+        var sci=this.props.sci;
+        var answers=this.props.answers;
+        var temp_Answer=null;
+        var keep=true;
+
         return(
             <article>
                 <h2>{this.props.title}</h2>
-                <div className="answer"
-                onClick={function(e){
-                    i = i + 1;
-                    e.preventDefault();
-                    this.props.onChangePage(data[i].id);
-                }.bind(this)}>
+                <div className="answer">
                     <div className="btn">
                         <ul>
-                            <li>{this.props.desc1}</li>
+                            <li>
+                                <button onClick={function(e){
+                                temp_Answer=1
+                                while(keep){
+                                if(sci===10){
+                                    this.props.onChangePage2();
+                                    break;
+                                }
+                                else {
+                                    this.props.onChangePage(data[sci].id);
+                                }
+                                if(Number(temp_Answer) === Number(answers[sci].answer)){
+                                    this.props.addScore();
+                                }
+                                keep=false;
+                                }}.bind(this)}>{this.props.desc1}</button>
+                            </li>
                             <br/>
-                            <li>{this.props.desc2}</li>
+                            <li>
+                                <button onClick={function(e){
+                                temp_Answer=2
+                                while(keep){
+                                if(sci===10){
+                                    this.props.onChangePage2();
+                                    break;
+                                }
+                                else {
+                                    this.props.onChangePage(data[sci].id);
+                                }
+                                if(Number(temp_Answer) === Number(answers[sci].answer)){
+                                    this.props.addScore();
+                                }
+                                keep=false;
+                                }}.bind(this)}>{this.props.desc2}</button>
+                            </li>
                             <br/>
-                            <li>{this.props.desc3}</li>
+                            <li>
+                                <button onClick={function(e){
+                                temp_Answer=3
+                                while(keep){
+                                if(sci===10){
+                                    this.props.onChangePage2();
+                                    break;
+                                }
+                                else {
+                                    this.props.onChangePage(data[sci].id);
+                                }
+                                if(Number(temp_Answer) === Number(answers[sci].answer)){
+                                    this.props.addScore();
+                                }
+                                keep=false;
+                                }}.bind(this)}>{this.props.desc3}</button>
+                            </li>
                             <br/>
-                            <li>{this.props.desc4}</li>
+                            <li>
+                                <button onClick={function(e){
+                                temp_Answer=4
+                                while(keep){
+                                if(sci===10){
+                                    this.props.onChangePage2();
+                                    break;
+                                }
+                                else {
+                                    this.props.onChangePage(data[sci].id);
+                                }
+                                if(Number(temp_Answer) === Number(answers[sci].answer)){
+                                    this.props.addScore();
+                                }
+                                keep=false;
+                                }}.bind(this)}>{this.props.desc4}</button>
+                            </li>
                         </ul>
                     </div>
                 </div>
