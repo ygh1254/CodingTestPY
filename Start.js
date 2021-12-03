fef67c08c000bdd0720039a1a5db7e8eb932c931
Yang Kyung Hun <88442236+ygh1254@users.noreply.github.com>
2021-12-03 23:36:40 +0900

    Update Start.js

--- Start.js
+++ Start.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 
 class Start extends Component{
     render(){
+        var sci=this.props.sci
+        var data=this.props.data
         return(
             <header>
                 <div onClick={function(e){
                     e.preventDefault();
-                    this.props.onChangePage();
+                    this.props.onChangePage(0);
+                    console.log(data[0])
                 }.bind(this)}>
                     <button>{this.props.sub}</button>
                 </div>
