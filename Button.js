4127b6ab63d0b9e12e00696fa7d03a9f42372b6f
Yang Kyung Hun <88442236+ygh1254@users.noreply.github.com>
2021-12-03 23:37:46 +0900

    Create Button.js

---
+++ Button.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+
+class Button extends Component{
+    render(){
+        var data=this.props.data;
+        var sci=this.props.sci;
+        var answers=this.props.answers;
+        var temp_Answer=null;
+        var keep=true;
+        
+        return(
+            <div>
+                <ul>
+                    <li>
+                        <button onClick={function(e){
+                        temp_Answer=1
+                        while(keep){
+                            if(sci===10){
+                                this.props.onChangePage2();
+                                break;
+                            }
+                            else {
+                                this.props.onChangePage(data[sci].id);
+                            }
+                            if(Number(temp_Answer) === Number(answers[sci].answer)){
+                                this.props.addScore();
+                            }
+                            keep=false;
+                        }}.bind(this)}>{this.props.desc1}</button>
+                    </li>
+                    <br/>
+                    <li>
+                        <button onClick={function(e){
+                        temp_Answer=2
+                        while(keep){
+                            if(sci===10){
+                                this.props.onChangePage2();
+                                break;
+                            }
+                            else {
+                                this.props.onChangePage(data[sci].id);
+                            }
+                            if(Number(temp_Answer) === Number(answers[sci].answer)){
+                                this.props.addScore();
+                            }
+                            keep=false;
+                        }}.bind(this)}>{this.props.desc2}</button>
+                    </li>
+                    <br/>
+                    <li>
+                        <button onClick={function(e){
+                        temp_Answer=3
+                        while(keep){
+                            if(sci===10){
+                                this.props.onChangePage2();
+                                break;
+                            }
+                            else {
+                                this.props.onChangePage(data[sci].id);
+                            }
+                            if(Number(temp_Answer) === Number(answers[sci].answer)){
+                                this.props.addScore();
+                            }
+                            keep=false;
+                        }}.bind(this)}>{this.props.desc3}</button>
+                    </li>
+                    <br/>
+                    <li>
+                        <button onClick={function(e){
+                        temp_Answer=4
+                        while(keep){
+                            if(sci===10){
+                                this.props.onChangePage2();
+                                break;
+                            }
+                            else {
+                                this.props.onChangePage(data[sci].id);
+                            }
+                            if(Number(temp_Answer) === Number(answers[sci].answer)){
+                                this.props.addScore();
+                            }
+                            keep=false;
+                        }}.bind(this)}>{this.props.desc4}</button>
+                    </li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default Button;
