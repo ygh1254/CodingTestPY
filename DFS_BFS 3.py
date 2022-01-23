370d5cccb1c2b90823333b22f23391d627927a3d
Yang Kyung Hun <ygh1254@hanyang.ac.kr>
2022-01-24 00:57:25 +0900

    음료수 얼려먹기

---
+++ DFS_BFS 3.py
@@ -0,0 +1,33 @@
+# DFS/BFS Problem 1
+from collections import deque
+#Width and Height is entered
+n, m = map(int, input().split())
+
+# Map data is initialized
+data = []
+
+# Map data is entered
+for _ in range(n):
+    data.append(list(map(int, input())))
+
+_data = data
+truetable = [[1] * m] * n
+
+
+def dfs(_data, truetable, x, y):
+    if _data == truetable or x < 0 or y < 0 or x == m or y == n:
+        # Do not execute
+        return 0
+    if _data[x][y] == 0:
+        _data[x][y] = 1
+        print(_data, x, y)
+    if _data[x][y+1] == 0:
+        # x or y exceed m or n => list index error occurs
+        dfs(_data, truetable, x, y+1)
+        print(_data, x, y)
+    if _data[x+1][y] == 0:
+        dfs(_data, truetable, x+1, y)
+        print(_data, x, y)
+
+
+dfs(_data, truetable, 0, 0)
